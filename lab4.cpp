8c1aa647c9b257528129c803f26acc4e776af7d3
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2017-06-17 19:11:16 -0400

    Lab 04 initial commit

---
+++ lab4.cpp
@@ -0,0 +1,63 @@
+#include <iostream> //standard input / output streams
+#include <iomanip>  //IO manipulators
+#include <fstream>  //file streams
+
+using namespace std;
+
+bool highlyRated(double);
+
+int main() {
+    int tn_records = 0; // total number of records
+    int tn_highly_rated_gifts = 0; // total number of records highly rated gifts
+    int request;
+    string giftname;
+    string status = "No"; //default we set the status to NO
+    double giftprice = 0.00;
+    double rating = 0.00;
+
+    ifstream inFile;
+    inFile.open("giftPrices.txt");
+
+    if (inFile.is_open()) {
+        //cout << "File is open " << endl;
+        cout << "GIFTS" << setw(20) << "PRICE" << setw(30) << "HIGHLY RATED\n";
+        cout << setfill('-') << setw(70) << "-" << endl;
+        while (!inFile.eof()) {
+            inFile >> giftname >> giftprice >> rating;
+            cout << setfill(' ');
+            request = highlyRated(rating);
+            //setting up the rating status
+            if (request == 0) {
+                status = "NO";
+            } else {
+                status = "YES";
+                tn_highly_rated_gifts++;
+            }
+            cout << giftname << setw(10) << setprecision(4) << giftprice << setw(10) << status << endl;
+            // computing the total number of gifts in that are on the file.
+            tn_records++;
+        }
+        cout << endl << endl;
+        cout << "There are " << setw(2) << tn_records << " on the list." << endl;
+        cout << "There are " << setw(2) << tn_highly_rated_gifts << " gifts with 5 stars on the list." << endl;
+        inFile.close();
+    } else {
+        cerr << "Cannot open the file." << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+/**
+ * @brief Return true if the item has 4 or more stars and false otherwise.
+ * @param rating Type bool
+ * @return true or false
+ * @date June 8th, 2016
+ * @author Suresh Melvin Sigera
+ */
+bool highlyRated(double rating) {
+    if (rating >= 4)
+        return true;
+    else
+        return false;
+}
