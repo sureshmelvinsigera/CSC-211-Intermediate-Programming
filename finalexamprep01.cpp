ba48ffcf6b92354ace403ffe2a5d090340d73649
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2017-07-25 12:51:49 -0400

    Final exam prep 01.

---
+++ finalexamprep01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 10;
+    int *ptr;
+    int j[] = {1,2,3,4,5,6,7,8,9,10};
+    int *jptr;
+
+    cout << "x is " << x << endl;
+
+    ptr = &x;
+
+    cout << "address of x is " << ptr << endl;
+
+    *ptr = 24;
+
+    cout << "Value of x is "  << x << endl;
+
+    jptr = j;
+
+    cout << " address of j index 0" << &j[0] << endl;
+    cout << jptr << endl;
+
+    cout << (jptr == &j[0]) << endl;
+
+    cout << "Print the full array " << endl;
+
+    for(int i=0; i<10; i++){
+        cout << jptr << " "  << *jptr << endl;
+        jptr++;
+    }
+
+    return 0;
+}
